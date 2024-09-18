698b78a709414c5d966cf01c33dca969839c18da
caueeugenio <caue_eugenio@hotmail.com>
2024-09-17 21:35:25 -0300

    updating branch

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  background-color: #ed145b;
+  color: white;
+  justify-content: center;
+  padding: 20px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 15px;
+`;
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/completed">Tarefas Concluídas</Link>
+        <Link to="/pending">Tarefas Pendentes</Link>
+      </Nav>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
